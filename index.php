3838751702389d498a051cb6d0ab91974ad36790
GianlucaBar <gianlucabarce@gmail.com>
2021-05-27 16:12:48 +0200

    completato

--- index.php
+++ index.php
@@ -1,28 +1,14 @@
 <?php 
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "dbhotel";
+    require_once __DIR__ . '/database.php';
 
-
-    // Connect
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn && $conn->connect_error) {
-
-        echo "Connection failed: " . $conn->connect_error;
-
-        die();
-    }
-
-    $sql = "SELECT `room_number`, `floor` FROM `stanze`";
+    $sql = "SELECT * FROM `stanze`";
     $result = $conn->query($sql);
 
+    $rooms = [];
     if ($result && $result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "Stanza N. ". $row['room_number']. " piano: ".$row['floor'];
+            $rooms[] = $row;
         }
     } elseif ($result) {
     echo "0 results";
@@ -31,5 +17,28 @@
     }
     $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dbhotel connect</title>
+</head>
+<body>
+    <h1>LISTA STANZE</h1>
+    <?php var_dump($room) ?>
+    <ul>
+        <?php foreach($rooms as $room) {?>
+            <li>
+                N. stanza: <?php echo $room['room_number'] ?>
+                Piano: <?php echo $room['floor'] ?>
+                <a href="room-info.php?id=<?php echo $room['id']?>">Dettagli stanza</a>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
 
 
